4f540548a912688873cf51a48fe00bae0ec425bf
Skizofreno <47854497+Skizofreno@users.noreply.github.com>
2024-03-21 02:06:28 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,6 @@ export default function Index({ posts, globalData }) {
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
         </h1>
-  <p className="description">
-  M-IPTV!
-  </p>
         <ul className="w-full">
           {posts.map((post) => (
             <li
