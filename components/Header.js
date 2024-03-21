012a1b4a9293c8a8198c0cb6f1bd726a77d0994f
Skizofreno <47854497+Skizofreno@users.noreply.github.com>
2024-03-21 04:27:43 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,11 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
+        <img
+  src="https://raw.githubusercontent.com/Skizofreno/nextjs-miptv/main/header-img.webp"
+  width="100%"
+  alt=""
+/>
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
