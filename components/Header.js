b6fa11f43e42989c1f76e30c4b444834bba757f5
Skizofreno <47854497+Skizofreno@users.noreply.github.com>
2024-03-22 02:46:50 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,15 +3,13 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header>
-    <div style={{ width: '100vw', objectFit:'cover' }}>
 {
-  <img
+  <img style={{ width: '100vw', objectFit:'cover' }}
   src="https://raw.githubusercontent.com/Skizofreno/nextjs-miptv/main/header-img.webp"
   alt="" 
   />
     
 }
-    </div>  
 
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
