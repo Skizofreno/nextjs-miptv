5ec5303fdd09b0d58d4220767f5e7a07af329d4c
Skizofreno <47854497+Skizofreno@users.noreply.github.com>
2024-03-22 03:00:56 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
 
 export default function Header({ name }) {
   return (
     <header>
-{
-  <img style={{ width: '100vw', height:'60vh', objectFit:'cover' }}
+
+    <Image
   src="https://raw.githubusercontent.com/Skizofreno/nextjs-miptv/main/header-img.webp"
-  alt="" 
-  />
-    
-}
+  alt="Description of the image"
+  width={100vw}
+  height={60vh}
+/>
+
+
 
       <p className="text-2xl dark:text-white text-left">
         <Link href="/">
