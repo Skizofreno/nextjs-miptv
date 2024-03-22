b35f8c964af9112afe3893be0f5ee912a1240d7b
Skizofreno <47854497+Skizofreno@users.noreply.github.com>
2024-03-22 02:12:34 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Header({ name }) {
   return (
-    <header className="pt-20 pb-12">
+    <header className="w-100vw">
         <img
   src="https://raw.githubusercontent.com/Skizofreno/nextjs-miptv/main/header-img.webp"
   width="100%"
