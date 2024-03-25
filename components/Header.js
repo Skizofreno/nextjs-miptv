05387e6f7403f15011deaddec02bf9e0b076366f
Skizofreno <47854497+Skizofreno@users.noreply.github.com>
2024-03-25 04:32:46 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,13 @@ export default function Header({ name }) {
   return (
     <header>
 
+  {
+    
+  }
 
+<div className=call-to-action>
+    <button className="call-button">FREE TRIAL</button>
+</div>
      
     </header>
   );
